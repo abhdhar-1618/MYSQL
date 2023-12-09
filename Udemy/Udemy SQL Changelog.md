71e782373d17dfed82dcabd803bfec012976bf31
abhdhar-1618 <abhikdhar2006@gmail.com>
2023-12-09 23:16:30 +0530

    Udemy SQL File updated

--- Udemy/Udemy SQL Changelog.md
+++ Udemy/Udemy SQL Changelog.md
@@ -1,3 +1,5 @@
 -- Udemy SQL Changelog
 
 08-12-2023 23:28:03 Udemy SQl course files added
+
+09-12-2023 23:15:13 Udemy SQL File updated
--- Udemy/Udemy_Couse_Query_Sheet.sql
+++ Udemy/Udemy_Couse_Query_Sheet.sql
@@ -1,5 +1,5 @@
 -- Show whole data of the table
-SELECT * FROM employees;
+SELECT * FROM products;
 
 -- Show specific columns from a table
 SELECT firstName,email,jobTitle,officeCode
@@ -24,6 +24,79 @@ SELECT lastName,firstName,email,jobTitle
 FROM employees
 WHERE jobTitle='Sales Rep'
 LIMIT 5;
+
+-- show all the customers IN US,Spain & Germany
+
+SELECT * FROM customers
+WHERE country='USA' OR country='France' OR country='Spain' OR country='Germany'
+ORDER BY country DESC;
+
+-- Same query as above without multiple usage of OR statements:
+
+SELECT *
+FROM customers
+WHERE country IN ('USA', 'France', 'Spain', 'Germany')
+ORDER BY country DESC;
+
+-- show all the customers who are not in US,Spain,France & Germany
+
+SELECT *
+FROM customers
+WHERE country NOT IN ('USA', 'France', 'Spain', 'Germany')
+ORDER BY country DESC;
+
+-- Show the count of top 7 distinct countries in customers table
+
+SELECT country, COUNT(*) AS total_count
+FROM customers
+GROUP BY country
+ORDER BY total_count DESC
+LIMIT 7;
+
+-- show all customers who has a credit limit between 10000 and 90000
+
+SELECT *
+FROM customers
+WHERE creditLimit BETWEEN 10000 AND 91000
+ORDER BY creditLimit DESC;
+
+-- what are the unique office codes in the employees table
+
+SELECT DISTINCT officeCode
+FROM employees;
+
+DESCRIBE employees;
+
+/* Renaming a column for dispaly purpose has been desribed in
+"Display data with Column Alias" video at 3:00 Minute mark*/
+
+/* Column data concatanation or merging columns have been discussed in
+video Display data with Column Alias" video at 4:55 Minute mark*/
+
+/* Data sorting is discussed in video "Display data in Sorting order" full video*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
